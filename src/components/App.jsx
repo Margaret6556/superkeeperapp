b537fa359ba14eed85151f49255ba62cdc4ff18a
Margaret Cezar <cezarlofellem@gmail.com>
2024-11-04 23:01:44 -0500

    updated changes on 14 files
    Added imgs and removed extra files

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Card from './Card';
+import contacts from '../contacts';
+
+export default function App() {
+  return (
+    <div className="heading">
+      <header className="heading">My Contacts</header>
+      <Card />
+    </div>
+  );
+}
+
+
